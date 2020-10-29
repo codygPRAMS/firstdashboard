9d951d9f8740b60077564ae40ecca46034918f1f
Cody <cody@pramsplus.com>
2020-10-29 14:38:57 -0600

    '3'

--- project/src/components/Navbar.js
+++ project/src/components/Navbar.js
@@ -37,6 +37,10 @@ function Navbar() {
                 })}
             </ul>
             /*test*/
+            /*test*/
+            /*test*/
+            /*test*/
+            /*test*/
           </nav>
           </IconContext.Provider>
         </>
